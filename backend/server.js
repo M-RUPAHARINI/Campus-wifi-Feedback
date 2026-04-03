6e69cdd039c3c7341d3f2c8ad46a4f7b62ca6d5a
M-RUPAHARINI <rupaharini2005@gmail.com>
2026-04-03 12:23:50 +0530

    Force fix CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://campus-wifi-feedback.vercel.app",
   credentials: true
 }));
 
